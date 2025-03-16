c704b7beb27760d0b2a2db037e0a51a8ed6c3f53
salmawagdy <salmawagdy227@gmail.com>
2025-03-16 05:27:54 +0200

    hi

--- src/app.controller.js
+++ src/app.controller.js
@@ -1,25 +1,11 @@
-<<<<<<< HEAD
-import { connectDB } from "./DB/connection.js"
-import authController from './modules/auth/auth.controller.js'
-import userController from './modules/user/user.controller.js'
-const bootstrap=(app, express)=>{
-app.use(express.json())
-app.get('/', (req, res) => res.send('Hello World!'))
-app.use("/auth",authController)
-app.use("/user",userController)
 
 
-connectDB()
-}
-
-
-
-export default bootstrap
-=======
 import { connectDB } from "../src/DB/connection.js";
 import productRouter from "./modules/product/product.controller.js";
 import categoryRouter from "./modules/category/category.controller.js";
 import subCategoryRouter from "./modules/subCategories/subCategories.controller.js";
+import authController from './modules/auth/auth.controller.js'
+import userController from './modules/user/user.controller.js'
 
 const bootstrap = (app, express) => {
   app.use(express.json());
@@ -30,6 +16,8 @@ const bootstrap = (app, express) => {
   app.use("/product", productRouter);
   app.use("/category", categoryRouter);
   app.use("/subCategory", subCategoryRouter);
+  app.use("/auth",authController)
+  app.use("/user",userController)
   app.all("*", (req, res) => {
     return res.status(404).json({ message: "Not found" });
   });
@@ -37,4 +25,3 @@ const bootstrap = (app, express) => {
 };
 
 export default bootstrap;
->>>>>>> 791278a4638196eebb9ec290a9926db209cd791c
