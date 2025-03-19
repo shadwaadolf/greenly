f2835e9401f4f4000571f304fa8f878eaf60a470
shadwaadolf <shadwaadolf5@gmail.com>
2025-03-19 03:28:03 +0200

    pushh

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import express from 'express'
-
-import bootstrap from './src/app.controller.js'
-const app = express()
-const port = 3000
-
-
-bootstrap(app,express)
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-=======
 import bootstrap from "./src/app.controller.js";
 import express from "express";
 //import bodyParser from "body-parser";
@@ -20,4 +8,3 @@ const port = 3000;
 app.use(express.json());
 bootstrap(app, express);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
->>>>>>> 791278a4638196eebb9ec290a9926db209cd791c
