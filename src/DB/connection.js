da802d4d32f89791b2f08a29a50bee347e1ce051
seifmuhammedd <seifamuhammed2@gmail.com>
2025-04-09 03:07:25 +0200

    conn

--- src/DB/connection.js
+++ src/DB/connection.js
@@ -3,18 +3,13 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    console.log("x11");
     await mongoose.connect(
       "mongodb://localhost:27017"
       // { serverSelectionTimeoutMS: 5000 }
     );
-    console.log("x1");
-
     console.log("db connected");
   } catch (err) {
-    console.log("x2");
     console.log("fail", err);
   }
-  console.log("x3");
 };
 // >>>>>>> 791278a4638196eebb9ec290a9926db209cd791c
