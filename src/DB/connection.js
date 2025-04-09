593a12b300306a9b7116f80d389962864db07815
seifmuhammedd <seifamuhammed2@gmail.com>
2025-04-09 02:20:43 +0200

    initial commit

--- src/DB/connection.js
+++ src/DB/connection.js
@@ -3,14 +3,18 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
+    console.log("x11");
     await mongoose.connect(
       "mongodb+srv://salma:123@cluster0.fohlf.mongodb.net/greenlasstt"
       // { serverSelectionTimeoutMS: 5000 }
     );
+    console.log("x1");
 
     console.log("db connected");
   } catch (err) {
+    console.log("x2");
     console.log("fail", err);
   }
+  console.log("x3");
 };
 // >>>>>>> 791278a4638196eebb9ec290a9926db209cd791c
